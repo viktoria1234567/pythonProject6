1eff7879b6741fd9d57d6b4102618a4c8e31bc95
user <vpredko@mail.ru>
2023-09-08 09:40:07 +0300

    task

---
+++ dom_1/task_1.py
@@ -0,0 +1,18 @@
+# Написать функцию на Python, которой передаются в качестве параметров команда и текст.
+# Функция должна возвращать True, если команда успешно выполнена
+# и текст найден в её выводе и False в противном случае.
+# Передаваться должна только одна строка, разбиение вывода использовать не нужно.
+
+import subprocess
+
+
+def test(comm, text):
+    result = subprocess.run(comm, shell=True, stdout=subprocess.PIPE, encoding='utf-8')
+    # print(result.stdout)
+    if text in result.stdout:
+        return True
+    else:
+        return False
+
+
+test("cat /etc/os-release", "22.04.1")
